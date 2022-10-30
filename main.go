c8832e219f861c47beb13cbdee2a876f630e477a
rahmatsyifana12 <adean435@gmail.com>
2022-10-30 14:01:50 +0700

    Add env file

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error getting from dotenv")
+	}
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-	  c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	  })
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world",
+		})
 	})
-	r.Run("localhost:5000")
-  }
+
+	r.Run("localhost:" + os.Getenv("PORT"))
+}
