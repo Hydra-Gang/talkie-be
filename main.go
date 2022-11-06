c7f62ce6f6b2bb2471ec9416d3ffbf6b454e6b70
rahmatsyifana12 <adean435@gmail.com>
2022-11-06 17:15:22 +0700

    feat: Add user model migration

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/Hydra-Gang/talkie-be/configs"
+	"github.com/Hydra-Gang/talkie-be/models"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -15,6 +17,11 @@ func main() {
 	if err != nil {
 		log.Fatal("failed to load .env file")
 	}
+
+	db,_ := configs.SetupDatabaseConnection()
+
+	// migrates all models
+	db.AutoMigrate(&models.User{})
 
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
