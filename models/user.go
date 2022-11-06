4ed328724057d0b9de877af16f3dcb47613bdf08
rahmatsyifana12 <adean435@gmail.com>
2022-11-06 17:09:52 +0700

    feat: Add user model (struct)

--- models/user.go
+++ models/user.go
@@ -1,1 +1,17 @@
 package models
+
+import "time"
+
+type User struct {
+	ID             string		`json:"id"`
+	Email          string		`json:"email"`
+	Password       string		`json:"password"`
+	FullName       string		`json:"full_name"`
+	Nickname       string		`json:"nickname"`
+	DOB            string		`json:"dob"`
+	Phone          string		`json:"phone"`
+	ProfilePicture string		`json:"profile_picture"`
+	RefreshToken   string		`json:"refresh_token"`
+	UpdatedAt      time.Time 	`json:"updated_at"`
+	CreatedAt      time.Time 	`json:"created_at"`
+}
