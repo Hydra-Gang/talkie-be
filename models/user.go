a1df19f21f6848112bb1ef9822733a267eab31d7
Alviannn <alvianndq@gmail.com>
2022-11-07 10:16:40 +0700

    feat(users): fix user model to use proper gorm tags and base model

--- models/user.go
+++ models/user.go
@@ -1,17 +1,17 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
 type User struct {
-	ID             string		`json:"id"`
-	Email          string		`json:"email"`
-	Password       string		`json:"password"`
-	FullName       string		`json:"full_name"`
-	Nickname       string		`json:"nickname"`
-	DOB            string		`json:"dob"`
-	Phone          string		`json:"phone"`
-	ProfilePicture string		`json:"profile_picture"`
-	RefreshToken   string		`json:"refresh_token"`
-	UpdatedAt      time.Time 	`json:"updated_at"`
-	CreatedAt      time.Time 	`json:"created_at"`
+	BaseModel
+	Email          string    `json:"email"`
+	Password       string    `gorm:"not null"`
+	FullName       string    `gorm:"not null" json:"fullName"`
+	Nickname       string    `json:"nickName"`
+	DOB            time.Time `gorm:"not null" json:"dob"`
+	Phone          string    `gorm:"not null" json:"phone"`
+	ProfilePicture string    `json:"profilePicture"`
+	RefreshToken   string    `json:"refreshToken"`
 }
