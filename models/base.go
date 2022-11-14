9f68f77c82b1fbfb620150b14df91a0993c98afc
Alviannn <alvianndq@gmail.com>
2022-11-14 21:25:41 +0700

    refactor(base-model): change ID to use `uuid`

--- models/base.go
+++ models/base.go
@@ -3,11 +3,12 @@ package models
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type BaseModel struct {
-	ID        string         `gorm:"primaryKey;type:uuid;default:gen_random_uuid()" json:"id"`
+	ID        uuid.UUID      `gorm:"primaryKey;type:uuid;default:gen_random_uuid()" json:"id"`
 	CreatedAt time.Time      `gorm:"autoCreateTime;not null" json:"createdAt"`
 	UpdatedAt time.Time      `gorm:"autoUpdateTime;not null" json:"updatedAt"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt"`
